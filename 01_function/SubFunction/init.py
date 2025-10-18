f25a954d9ddddf7157fa98c51e31f130e3b8ec26
Thana-site <tmeengurn@gmail.com>
2025-10-18 10:10:18 +0700

    Update init.py

--- 01_function/SubFunction/init.py
+++ 01_function/SubFunction/init.py
@@ -1,22 +1,21 @@
 """
-RC Beam Analysis Sub-Functions
-Professional structural engineering tools for ACI 318 compliant design
+RC Beam Analysis Application Modules
+ACI 318 Compliant Structural Analysis
 """
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __author__ = "Structural Engineering Team"
 
-# Import all modules for easy access
-from . import material_config
-from . import analysis_core
-from . import diagram_plot
-from . import input_tables
-from . import ui_layout
+from . import material_properties
+from . import analysis_engine
+from . import visualization
+from . import input_components
+from . import ui_components
 
 __all__ = [
-    'material_config',
-    'analysis_core',
-    'diagram_plot',
-    'input_tables',
-    'ui_layout'
+    'material_properties',
+    'analysis_engine',
+    'visualization',
+    'input_components',
+    'ui_components'
 ]
